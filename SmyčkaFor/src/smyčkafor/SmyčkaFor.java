ef5d959b2260e73bfab35ad6eb3fa57562701458
petra.podhajska <petra.podhajska@B110-07.pslib.cz>
2015-09-30 10:00:05 +0200

    funkční kód

---
+++ SmyčkaFor/src/smyčkafor/SmyčkaFor.java
@@ -0,0 +1,33 @@
+
+package smyčkafor;
+
+
+public class SmyčkaFor {
+
+ 
+    public static void main(String[] args) {
+   
+     for (int i=1; i <10; i++) {
+         System.out.print (i+"");
+          }
+         System.out.println();
+         
+         //násobilka 3
+         for (int i=1; i <=10; i++) {
+              System.out.print (i*3+" ");
+         }
+         System.out.println();
+         
+          for (int i=1; i <=10; i++) {
+              System.out.print (i*1+" ");
+         }
+         System.out.println();
+         
+         for (int i=1; i <=10; i++) {
+              System.out.print (i*2+" ");
+         }
+         System.out.println();
+    }
+    }
+    
+
